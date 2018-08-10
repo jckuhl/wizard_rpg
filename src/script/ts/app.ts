7e108eb52281e4c30b1fabfc1b3132319c9e26ec
Jonathan Kuhl <jonathankuhl@Jonathans-MacBook-Pro-2.local>
2018-08-09 21:14:44 -0400

    started basic ui logic

--- src/script/ts/app.ts
+++ src/script/ts/app.ts
@@ -4,15 +4,22 @@ import Wizard from './../ts/wizard';
 new Vue({
     el: '#app',
     data: {
-        wizard: new Wizard('merlin'),
-        name: 'fuck off'
+        wizard: null,
+        name: null,
+        invalidName: true,
+        startGame: false,
     },
     methods: {
-        createWizard() {
-
+        validateName() {
+            if(this.name.length >= 2) this.invalidName = false;
+            else this.invalidName = true;
+        },
+        createWizard(event: Event) {
+            event.preventDefault();
+            this.name = this.name.slice(0,1).toUpperCase() + this.name.slice(1);
+            this.wizard = new Wizard(this.name);
+            this.startGame = true;
+            console.log(this.wizard);
         }
-    },
-    created: function() {
-        console.log('hello');
     }
 })
